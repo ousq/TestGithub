5aceb81409a0a5f21a99b5d326e6dff269bb32c4
ousq <Europe_an@163.com>
2017-07-07 10:27:48 +0800

    text

--- app/src/main/java/com/osx/testgithub/MainActivity.java
+++ app/src/main/java/com/osx/testgithub/MainActivity.java
@@ -2,6 +2,8 @@ package com.osx.testgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"測試",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
